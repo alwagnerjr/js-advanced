824209c62aed07bfade69df15e17d973237e928e
alwagner <shelenokasya@gmail.com>
2020-12-01 20:44:25 +1000

    Добавлены стили для всех элементов

---
+++ script.js
@@ -0,0 +1,31 @@
+const goods = [
+  { title: "PS5", price: 30000, id: 1},
+  { title: "IPhone 12", price: 100000, id: 2},
+  { title: "Shirt", price: 1000, id: 3},
+  { title: "XBOX", price: 30000, id: 4},
+  { title: "Boots", price: 5000, id: 5},
+];
+
+const main = () => {
+  const createGoodsItemTemplate = ({
+    title,
+    price,
+    id,
+  }) => `<div class="goods-item">
+    <img src="img/item${id}.jpg" alt="picture">
+    <h3>${title}</h3>
+    <p>${price}</p>
+</div>`;
+
+  const renderGoods = (items) => {
+    const goodsItemsList = items
+      .map((item) => createGoodsItemTemplate(item))
+      .join("");
+    console.log(goodsItemsList);
+    document.querySelector(".goods-list").innerHTML = goodsItemsList;
+  };
+
+  renderGoods(goods);
+};
+
+window.addEventListener("load", main);
